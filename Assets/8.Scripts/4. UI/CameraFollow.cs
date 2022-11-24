591b8fe403da40368c67b7143d8ddcb625793ce8
easyecec <48100098+easyecec@users.noreply.github.com>
2022-11-24 02:13:19 -0600

    Camera Fixes

--- Assets/8.Scripts/4. UI/CameraFollow.cs
+++ Assets/8.Scripts/4. UI/CameraFollow.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     [SerializeField] private float followSpeed = 2f;
-    [SerializeField] private float yOffset = 1f;
+    [SerializeField] private float yOffset = 0.3f;
+    [SerializeField] private float xOffset = 7f;
+    [SerializeField] private float zOffset = -10f;
     [SerializeField] private Transform target;
 
     void Update()
     {
-        Vector3 newPos = new Vector3(target.position.x, target.position.y + yOffset, -10f);
+        Vector3 newPos = new Vector3(target.position.x + xOffset, target.position.y + yOffset, zOffset);
         transform.position = Vector3.Slerp(transform.position, newPos, followSpeed * Time.deltaTime);
     }
 }
