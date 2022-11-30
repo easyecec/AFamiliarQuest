4a01e35e2442a4a1cf2b148f81fb84e75630e78b
A01632202 <48100098+easyecec@users.noreply.github.com>
2022-11-29 18:23:53 -0600

    Multiplayer scripts added and modified

--- Assets/8.Scripts/5. Multiplayer/StartNetworkGame.cs
+++ Assets/8.Scripts/5. Multiplayer/StartNetworkGame.cs
@@ -1,23 +1,20 @@
 using Fusion;
+using Fusion.Sockets;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 
-public class StartNetworkGame : MonoBehaviour
+public class StartNetworkGame : MonoBehaviour, INetworkRunnerCallbacks
 {
     [SerializeField] private NetworkRunner _networkRunner;
     [SerializeField] private string _roomName;
     [SerializeField] private string _sceneName;
-
-    // private void Awake()
-    // {
-    //      DontDestroyOnLoad(this);
-    //}
+    private UnityEvent<NetworkRunner, PlayerRef> OnPlayerJoinedEvent;
     async void StartNewGame(GameMode mode)
     {
-        // var gameArgs = new StartGameArgs();
-        // gameArgs.GameMode= mode;
         await _networkRunner.StartGame(new StartGameArgs()
         {
             GameMode = mode,
@@ -26,7 +23,6 @@ public class StartNetworkGame : MonoBehaviour
             SceneManager = gameObject.AddComponent<NetworkSceneManagerDefault>()
         });
         _networkRunner.SetActiveScene(_sceneName);
-
     }
 
     public void StartGameAsHost()
@@ -39,8 +35,91 @@ public class StartNetworkGame : MonoBehaviour
         StartNewGame(GameMode.Client);
     }
 
-    public void PlayerJoined(PlayerRef player)
+    public void OnPlayerJoined(NetworkRunner runner, PlayerRef player)
     {
-        Debug.Log("Player Joined");
+        if (runner.IsServer)
+        {
+            Debug.Log("Connected as Host");
+            OnPlayerJoinedEvent?.Invoke(runner, player);
+        }
+        else
+        {
+            Debug.Log("Connected as Client");
+        }
     }
+
+    public void OnPlayerLeft(NetworkRunner runner, PlayerRef player)
+    {
+    }
+
+    public void OnInput(NetworkRunner runner, NetworkInput input)
+    {
+
+    }
+
+    public void OnInputMissing(NetworkRunner runner, PlayerRef player, NetworkInput input)
+    {
+       
+    }
+
+    public void OnShutdown(NetworkRunner runner, ShutdownReason shutdownReason)
+    {
+
+    }
+
+    public void OnConnectedToServer(NetworkRunner runner)
+    {
+   
+    }
+
+    public void OnDisconnectedFromServer(NetworkRunner runner)
+    {
+
+    }
+
+    public void OnConnectRequest(NetworkRunner runner, NetworkRunnerCallbackArgs.ConnectRequest request, byte[] token)
+    {
+
+    }
+
+    public void OnConnectFailed(NetworkRunner runner, NetAddress remoteAddress, NetConnectFailedReason reason)
+    {
+
+    }
+
+    public void OnUserSimulationMessage(NetworkRunner runner, SimulationMessagePtr message)
+    {
+
+    }
+
+    public void OnSessionListUpdated(NetworkRunner runner, List<SessionInfo> sessionList)
+    {
+
+    }
+
+    public void OnCustomAuthenticationResponse(NetworkRunner runner, Dictionary<string, object> data)
+    {
+
+    }
+
+    public void OnHostMigration(NetworkRunner runner, HostMigrationToken hostMigrationToken)
+    {
+
+    }
+
+    public void OnReliableDataReceived(NetworkRunner runner, PlayerRef player, ArraySegment<byte> data)
+    {
+
+    }
+
+    public void OnSceneLoadDone(NetworkRunner runner)
+    {
+
+    }
+    public void OnSceneLoadStart(NetworkRunner runner)
+    {
+
+    }
+
+
 }
