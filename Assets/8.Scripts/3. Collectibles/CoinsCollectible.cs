47bdba30a7ba00087db581e6178f90d9a4f3fb98
easyecec <48100098+easyecec@users.noreply.github.com>
2022-12-01 02:35:14 -0600

    First Sound Try - Not Working

--- Assets/8.Scripts/3. Collectibles/CoinsCollectible.cs
+++ Assets/8.Scripts/3. Collectibles/CoinsCollectible.cs
@@ -12,6 +12,8 @@ public class CoinsCollectible : MonoBehaviour
         {
             playerManager = other.gameObject.GetComponent<PlayerManager>();
 
+            FindObjectOfType<AudioManager>().Play("Coin");
+
             playerManager.Coins += 1;
             Destroy(gameObject);
         }
